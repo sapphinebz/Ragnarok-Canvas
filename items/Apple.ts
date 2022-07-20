157441ecc9e17067dbe2114cab1d885b2a005983
sapphinebz <sapphinebz@gmail.com>
2022-07-20 17:13:06 +0700

    add healing audio

--- items/Apple.ts
+++ items/Apple.ts
@@ -1,15 +1,20 @@
 import { Monster } from '../monsters/Monster';
+import { loadHealAudio } from '../sounds/heal-effect';
 import { randomMinMax } from '../utils/random-minmax';
 import { appleImage } from './images/apple-image';
 import { Item } from './Item';
 
 export class Apple extends Item {
   usable = true;
+  useAudio = loadHealAudio();
   constructor(public canvas: HTMLCanvasElement) {
     super(canvas, appleImage);
+
+    this.useAudio.volume = 0.05;
   }
   useWith(monster: Monster): void {
     const hp = randomMinMax(12, 25);
     monster.restoreHp(hp);
+    this.useAudio.play();
   }
 }
--- items/RedPotion.ts
+++ items/RedPotion.ts
@@ -1,15 +1,19 @@
 import { Monster } from '../monsters/Monster';
+import { loadHealAudio } from '../sounds/heal-effect';
 import { randomMinMax } from '../utils/random-minmax';
 import { redPotionImage } from './images/red-potion-image';
 import { Item } from './Item';
 
 export class RedPotion extends Item {
   usable = true;
+  useAudio = loadHealAudio();
   constructor(public canvas: HTMLCanvasElement) {
     super(canvas, redPotionImage);
+    this.useAudio.volume = 0.05;
   }
   useWith(monster: Monster): void {
     const hp = randomMinMax(45, 65);
     monster.restoreHp(hp);
+    this.useAudio.play();
   }
 }
