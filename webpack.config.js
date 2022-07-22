58500ebf7331b4e26b4fb2c26ab18029f4cb1b8a
sapphinebz <sapphinebz@gmail.com>
2022-07-22 07:47:10 +0700

    change folder compile to docs

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   mode: "production",
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "docs"),
   },
   module: {
     rules: [
