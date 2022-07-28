84a6018f551ddfe909652c1460b8447c52b84ce0
sapphinebz <sapphinebz@gmail.com>
2022-07-28 15:30:59 +0700

    optimize pure function - score drawer and add heal audio

--- skills/Heal.ts
+++ skills/Heal.ts
@@ -1,13 +1,18 @@
 import { Monster } from "../monsters/Monster";
+import { loadHealAudio } from "../sounds/heal-effect";
 import { Skill } from "./Skill";
 
 export class Heal extends Skill {
   passive: boolean = false;
+  useAudio = loadHealAudio();
   constructor(public level: number) {
     super();
+
+    this.useAudio.volume = 0.05;
   }
   useWith(monster: Monster, toMonster: Monster) {
     /** formula Heal */
+    this.useAudio.play();
     toMonster.restoreHp(toMonster.maxHp * 0.05 * this.level);
   }
 }
